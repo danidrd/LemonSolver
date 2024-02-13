8a3a752b9be4e6c5712e8a91e7411d54bab4f79f
danidrd <danielecaliandro@gmail.com>
2024-02-13 12:53:16 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+#INSTALL lemon
+cd lemon-development
+mkdir build
+cd build
+cmake -DLEMON_ENABLE_GLPK=NO -DLEMON_ENABLE_COIN=NO -DLEMON_ENABLE_ILOG=NO ..
+make
+#make check
+
+#cp libemon & config.h to /LemonSolver/lib & /LemonSolver/include
+cd ../..
+
+mkdir -p lib
+mkdir -p include
+mkdir -p include/lemon
+mkdir -p include/lemon/bits
+mkdir -p include/lemon/concepts
+
+cp lemon-development/build/lemon/libemon.a lib
+cp lemon-development/build/lemon/config.h include
+
+
+cp lemon-development/lemon/*.h include/lemon
+cp lemon-development/lemon/bits/*.h include/lemon/bits
+cp lemon-development/lemon/concepts/*.h include/lemon/concepts
