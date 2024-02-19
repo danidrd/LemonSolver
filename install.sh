fcada4320ddae7709ab7b38fc78ce2155819bdb3
Daniele Caliandro <danielecaliandro.dev@gmail.com>
2024-02-19 16:18:39 +0100

    added download section

--- install.sh
+++ install.sh
@@ -8,8 +8,19 @@ cmake -DLEMON_ENABLE_GLPK=NO -DLEMON_ENABLE_COIN=NO -DLEMON_ENABLE_ILOG=NO ..
 make
 #make check
 
-#COPY all files .h and .a to LemonSolver
+
 cd ../..
+#DOWNLOAD boost, netCDF, eigen
+wget https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source/boost_1_84_0.tar.gz
+tar -xvzf boost_1_84_0.tar.gz
+rm boost_1_84_0.tar.gz
+
+git clone https://gitlab.com/libeigen/eigen.git
+
+wget https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz
+tar -xvzf netcdf-c-4.9.2.tar.gz
+rm netcdf-c-4.9.2.tar.gz
+#COPY all files .h and .a to LemonSolver
 
 mkdir -p lib
 mkdir -p include
